9ebf192f358af81f6ac44ffa3550797a483a1013
victor <victor.murithi@student.moringaschool.com>
2023-10-04 14:47:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // iterate from the beginning of the string to the middle of the string
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    // compare the letter we're iterating over to the corresponding letter at the end of the string
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      // if the letters don't match, return false
+      return false;
+    }
+  }
+
+  // if we reach the middle, and all the letters match, return true
+  return true;
 }
+
 
 /* 
   Add your pseudocode here
+  Iterate from the beggining to the middle 
+  Check each leter to the corresponding letter from the end 
+  if any letters dont match return false
 */
 
 /*
@@ -15,6 +30,26 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
